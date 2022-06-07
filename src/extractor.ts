11eed44697cd56d58fa5b90f9905ea075faa6ede
michaelballos <ballos.michael@gmail.com>
2022-06-07 12:39:16 -0500

    Transfer to mac mini pt 3

--- src/extractor.ts
+++ src/extractor.ts
@@ -6,10 +6,19 @@ module.exports = (async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://archillect.com/archive/');
-    await page.click('img.thumb[src]');
-    await page.$eval('img[id=ii]', img => {
-      const link = img.getAttribute('src');
-      console.log('LINK:',link);
+    await page.$$('img.thumb[src]').then(async (imagePreviews) => {
+      const urls = imagePreviews.map(async (img) => {
+        await img.click();
+        await page.waitForSelector('#ii');
+        const url = await page.$eval('img[src="ii"]', (el) => el.src);
+      });
+      const result = await Promise.all(urls);
+    });
+    await page.waitForSelector('#ii');
+    await page.$$eval('img[id="ii"]', (images) => {
+      images.map((image) => {
+        console.log('src:', image.src);
+      });
     });
     await browser.close();
   } catch (e) {
