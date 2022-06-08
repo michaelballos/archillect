1122680eb3aca6da344f6aa8c726727cd562f51b
michaelballos <ballos.michael@gmail.com>
2022-06-07 20:51:11 -0500

    Async crawler

--- src/extractor.ts
+++ src/extractor.ts
@@ -6,20 +6,29 @@ module.exports = (async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://archillect.com/archive/');
-    await page.$$('img.thumb[src]').then(async (imagePreviews) => {
-      const urls = imagePreviews.map(async (img) => {
-        await img.click();
-        await page.waitForSelector('#ii');
-        const url = await page.$eval('img[src="ii"]', (el) => el.src);
-      });
-      const result = await Promise.all(urls);
+    const numberOfImages = await page.$$eval('img.thumb[src]', (elements) => {
+      return elements.length;
     });
-    await page.waitForSelector('#ii');
-    await page.$$eval('img[id="ii"]', (images) => {
-      images.map((image) => {
-        console.log('src:', image.src);
-      });
-    });
+
+    const imageLinks = [];
+    await page.click('a[class="post"]');
+    for (let i=0; i<numberOfImages; i++) {
+      await page.waitForSelector('img[id="ii"]');
+      const image = await page.$('img[id="ii"]');
+      const src = (await image.getProperty('src'))._remoteObject.value;
+      console.log('src: ', src);
+      const imageLink = src.replace('66', '64');
+      if (!imageLinks.includes(imageLink)) {
+        imageLinks.push(imageLink);
+      }
+      try {
+      await page.click('a[id="imgolder"]');
+      await page.waitForSelector('img[id="ii"]');
+      } catch (e) {
+        console.log('no more images');
+      }
+    }
+    console.log('LINKS:', imageLinks);
     await browser.close();
   } catch (e) {
     console.log(e, 'Page did not load');
